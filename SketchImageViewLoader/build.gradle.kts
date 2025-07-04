dc1b2952ac3e8b37f3bf6f32a6aad64d6c4b23e5
yangyufei0928 <126234328+yangyufei0928@users.noreply.github.com>
2025-07-04 14:30:05 +0800

    Update build.gradle.kts

--- SketchImageViewLoader/build.gradle.kts
+++ SketchImageViewLoader/build.gradle.kts
@@ -1,84 +1,32 @@
-import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
-import org.gradle.api.JavaVersion
-import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
-
 plugins {
     id("com.android.library")
     id("kotlin-android")
 }
 
-group = "com.github.mikaelzero"
-setupLibraryModule {
-    namespace = "net.mikaelzero.mojito.view.sketch"
-    defaultConfig {
-        minSdk = 16
-    }
-}
-fun Project.setupLibraryModule(block: LibraryExtension.() -> Unit = {}) {
-    setupBaseModule<LibraryExtension> {
-        testOptions {
-            unitTests.isIncludeAndroidResources = true
-        }
-        block()
-    }
-}
-
-
 android {
-    compileSdkVersion(34)
+    compileSdk = 34
+    namespace = "net.mikaelzero.mojito.view.sketch"
 
     buildFeatures {
-        buildConfig = true
+        buildConfig = true  // ✅ 打开 BuildConfig 生成
     }
+
     defaultConfig {
         minSdk = 16
         targetSdk = 34
+
         buildConfigField("String", "VERSION_NAME", "\"1.0.0\"")
         buildConfigField("int",    "VERSION_CODE", "1")
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
 
-
-fun Project.setupAppModule(block: BaseAppModuleExtension.() -> Unit = {}) {
-    setupBaseModule<BaseAppModuleExtension> {
-        defaultConfig {
-            versionCode = 1
-            versionName = "1.0"
-            vectorDrawables.useSupportLibrary = true
-        }
-        block()
+    kotlinOptions {
+        jvmTarget = "17"
     }
-}
-
-inline fun <reified T : BaseExtension> Project.setupBaseModule(crossinline block: T.() -> Unit = {}) {
-    extensions.configure<BaseExtension>("android") {
-        compileSdkVersion(34)
-        defaultConfig {
-            minSdk = 16
-            targetSdk = 34
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-        kotlinOptions {
-            jvmTarget = "17"
-        }
-        (this as T).block()
-    }
-}
-
-fun BaseExtension.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
 }
 
 dependencies {
@@ -90,4 +38,3 @@ dependencies {
     implementation(libs.androidx.exifinterface)
     implementation(project(":mojito"))
 }
-
